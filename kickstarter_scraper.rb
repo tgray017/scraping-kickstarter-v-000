29b3e2b2a77ebd8962392d56e14d1334f3c6843b
tgray017 <tgray017@gmail.com>
2018-11-13 14:42:24 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,8 +18,6 @@ def create_project_hash
       :description => project.css("p.bbcard_blurb").text
       :location => project.css("div.project-card .project-meta li a .location-name").text
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-      
-      
     }
     
   end
